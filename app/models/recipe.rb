6f3c5607e7642757289720be95a91405f0e5245d
HankLiu5 <liu19901124@gmail.com>
2015-10-19 16:17:02 -0700

    Final product.

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,13 +1,13 @@
 ENV["FOOD2FORK_KEY"] = "7cba46c16a69ad6ce8a2126e55a52391"
-
+ENV['FOOD2FORK_SERVER_AND_PORT'] = 'food2fork.com:80'
 class Recipe
   include HTTParty
 
   key_value = ENV['FOOD2FORK_KEY']
-  hostport = ENV
+  hostport = ENV['FOOD2FORK_SERVER_AND_PORT'] || 'www.food2fork.com'
 
-  base_uri "http://food2fork.com/api"
-  default_params key: ENV["FOOD2FORK_KEY"]
+  base_uri "http://#{hostport}/api"
+  default_params key: key_value
   format :json
 
   def self.for (keyword)
